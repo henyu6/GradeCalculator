68eb1ff30bac5c496ce6f6f49b108bea9b50fa7d
Henry <henyu6@gmail.com>
2016-03-09 17:32:03 -0800

    display screen gui set

---
+++ app/src/main/java/com/example/henry/gradecalculator/DisplayActivity.java
@@ -0,0 +1,17 @@
+package com.example.henry.gradecalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class DisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display);
+    }
+
+    public void returnHomeBtn(View view) {
+    }
+}
